bd731e22807d2ded664b02cfc96b330ad754a16c
aby pious <abypious3@gmail.com>
2024-02-05 15:04:44 +0530

    buttons added

---
+++ lib/scenarios/ag_scn.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ag_scn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page 7'),
+      ),
+      body: Center(
+        child: Text('Content for Page 7'),
+      ),
+    );
+  }
+}
